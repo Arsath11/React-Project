479b61806fe724afd83eb2bc53c94d561e18c431
Arsath-Ahamed <ahamedarsath580@gmail.com>
2024-06-26 16:04:20 +0530

    header

---
+++ react-project1/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import Header from './header/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+ <Header />
+    </>
+  )
+}
+
+export default App
